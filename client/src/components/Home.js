2a2925a59d2d4c074128608d239a689de940da16
Bappy71 <19103076@iubat.edu>
2023-01-26 02:05:09 +0600

    Initial commit

---
+++ client/src/components/Home.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='mt-5'>
+            <div className="container">
+                <div className="add_btn mt-2">
+                    <button className='btn btn-primary'>Add Data</button>
+                </div>
+
+
+
+                <table class="table mt-2">
+                    <thead >
+                        <tr className=' table-dark'>
+                            <th scope="col">Id</th>
+                            <th scope="col">User Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Job</th>
+                            <th scope="col">Number</th>
+                            <th scope="col"></th>
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Gk</td>
+                            <td>Gk@gmail.com</td>
+                            <td>Footballer</td>
+                            <td>87</td>
+                            
+                            <td className='d-flex justify-content-between'>
+                                <button className='btn btn-success'>Read</button>
+                                <button className='btn btn-warning'>Update</button>
+                                <button className='btn btn-danger'>Delete</button>
+                            </td>
+                        </tr>
+                 
+                       
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
