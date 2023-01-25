2f5a08abcda23c6ee0e1d1ce7025d4d93cd3b653
Bappy71 <19103076@iubat.edu>
2023-01-26 03:30:34 +0600

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
-import Navbar from './components/Navbar';
-import Home from './components/Home';
- 
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Register from './components/Register'
+import { Switch, Route } from "react-router-dom"
+
 
 function App() {
   return (
     <div className="App">
-        <Navbar/>
-        <Home/>
+
+
+      <Navbar />
+      <Home />
+      <Register />
+
     </div>
   );
 }
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -36,6 +36,20 @@ const Home = () => {
                                 <button className='btn btn-danger'>Delete <i class="fa-solid fa-trash"></i></button>
                             </td>
                         </tr>
+                        
+                        <tr>
+                            <th scope="row">1</th>
+                            <td>Gk</td>
+                            <td>Gk@gmail.com</td>
+                            <td>Footballer</td>
+                            <td>87</td>
+                            
+                            <td className='d-flex justify-content-between'>
+                                <button className='btn btn-success'>Read <i class="fa-solid fa-eye"></i></button>
+                                <button className='btn btn-warning'>Update <i class="fa-solid fa-wrench"></i></button>
+                                <button className='btn btn-danger'>Delete <i class="fa-solid fa-trash"></i></button>
+                            </td>
+                        </tr>
                  
                        
                     </tbody>
