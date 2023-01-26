17fc2e513142d995b095785fb77e328d794afb6b
Bappy71 <19103076@iubat.edu>
2023-01-26 23:14:52 +0600

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,43 @@
+import {
+  Routes,
+  Route,
+  Link, Switch
+} from "react-router-dom";
+
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Register from './components/Register'
-import { Switch, Route } from "react-router-dom"
+
+
+
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
+
+   
+        <Navbar />
+        <Switch>
+            <Route path= "/" component={Home}/>
+        </Switch>
+
+        <Home />
+        <Register />
+
+        </div> 
 
 
-      <Navbar />
-      <Home />
-      <Register />
 
-    </div>
+  
+
+
+
+
+
   );
 }
 
