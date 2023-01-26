61a4ea3ce8a7339ec665d9e22db70e3b5c2c840b
Bappy71 <19103076@iubat.edu>
2023-01-27 02:52:42 +0600

    Initial commit

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -5,26 +5,26 @@ const Register = () => {
 
     const [inpval, setINP] = useState(
         {
-            name : " ",
+            name: " ",
             email: " ",
-            age : " ",
-            mobile : " ",
-            work : " ",
-            address : " ",
-            desc : " "
+            age: " ",
+            mobile: " ",
+            work: " ",
+            address: " ",
+            desc: " "
         }
     )
 
-const setData = (e)=>{
-    console.log(e.target.value);
-    const {name, value} = e.target;
-    setINP((preval)=>{
-        return {
-            ...preval,
-            [name]: value
-        }
-    })
-}
+    const setData = (e) => {
+        console.log(e.target.value);
+        const { name, value } = e.target;
+        setINP((preval) => {
+            return {
+                ...preval,
+                [name]: value
+            }
+        })
+    }
 
 
 
@@ -36,33 +36,33 @@ const setData = (e)=>{
                 <div className="row">
                     <div class="mb-3 col-lg-6 col-md-6 col-12 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputEmail1" class="form-label">Name</label>
-                        <input type="email" onChange={setData} value={inpval.name} name='name' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                        <input type="text" onChange={setData} value={inpval.name} name='name' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     </div>
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Email</label>
-                        <input type="password" value={inpval.email} name='email' class="form-control" id="exampleInputPassword1" />
+                        <input type="email" onChange={setData} value={inpval.email} name='email' class="form-control" id="exampleInputPassword1" />
                     </div>
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Age</label>
-                        <input type="password" value={inpval.age} name='age' class="form-control" id="exampleInputPassword1" />
+                        <input type="text" onChange={setData} value={inpval.age} name='age' class="form-control" id="exampleInputPassword1" />
                     </div>
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Mobile</label>
-                        <input type="password" value={inpval.mobile} name='mobile' class="form-control" id="exampleInputPassword1" />
+                        <input type="number" onChange={setData} value={inpval.mobile} name='mobile' class="form-control" id="exampleInputPassword1" />
                     </div>
 
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Work</label>
-                        <input type="password" value={inpval.work} name='work' class="form-control" id="exampleInputPassword1" />
+                        <input type="text" onChange={setData} value={inpval.work} name='work' class="form-control" id="exampleInputPassword1" />
                     </div>
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Address</label>
-                        <input type="password" value={inpval.address} name='address' class="form-control" id="exampleInputPassword1" />
+                        <input type="text" onChange={setData} value={inpval.address} name='address' class="form-control" id="exampleInputPassword1" />
                     </div>
                     <div class="mb-3 col-lg-12 col-md-12 col-12">
 
                         <label for="exampleInputPassword1" class="form-label">Discription</label>
-                        <textarea name="desc" value={inpval.desc} className='form-control' id="" cols="30" rows="6"></textarea>
+                        <textarea name="desc" value={inpval.desc} onChange={setData} className='form-control' id="" cols="30" rows="6"></textarea>
                     </div>
 
                     <button type="submit" class="btn btn-primary">Submit</button>
