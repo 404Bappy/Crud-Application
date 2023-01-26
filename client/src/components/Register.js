5f64072ede3cd318cfe400197e2fd53aab31c2be
Bappy71 <19103076@iubat.edu>
2023-01-27 02:43:02 +0600

    Initial commit

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,45 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const Register = () => {
+
+    const [inpval, setINP] = useState(
+        {
+            name : " ",
+            email: " ",
+            age : " ",
+            mobile : " ",
+            work : " ",
+            address : " ",
+            desc : " "
+        }
+    )
+
+const setData = (e)=>{
+    console.log(e.target.value);
+    const {name, value} = e.target;
+    setINP((preval)=>{
+        return {
+            ...preval,
+            [name]: value
+        }
+    })
+}
+
+
+
     return (
         <div className='container'>
             <NavLink to="/">home</NavLink>
 
-            <form>
+            <form className='mt-4'>
                 <div className="row">
                     <div class="mb-3 col-lg-6 col-md-6 col-12 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputEmail1" class="form-label">Name</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                        <input type="email" onChange={setData} value={inpval.name} name='name' class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
                     </div>
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Email</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <input type="password" value={inpval.email} name='email' class="form-control" id="exampleInputPassword1" />
                     </div>
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Age</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <input type="password" value={inpval.age} name='age' class="form-control" id="exampleInputPassword1" />
                     </div>
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Mobile</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <input type="password" value={inpval.mobile} name='mobile' class="form-control" id="exampleInputPassword1" />
                     </div>
-                   
+
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Work</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <input type="password" value={inpval.work} name='work' class="form-control" id="exampleInputPassword1" />
                     </div>
                     <div class="mb-3 col-lg-6 col-md-6 col-12">
                         <label for="exampleInputPassword1" class="form-label">Address</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" />
+                        <input type="password" value={inpval.address} name='address' class="form-control" id="exampleInputPassword1" />
                     </div>
                     <div class="mb-3 col-lg-12 col-md-12 col-12">
-                        
+
                         <label for="exampleInputPassword1" class="form-label">Discription</label>
-                        <textarea name="" className='form-control' id="" cols="30" rows="10"></textarea>
+                        <textarea name="desc" value={inpval.desc} className='form-control' id="" cols="30" rows="6"></textarea>
                     </div>
-                   
+
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </div>
             </form>
