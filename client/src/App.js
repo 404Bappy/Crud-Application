1165dc7daea70ad4612e9d6bb9774c4b623ac382
Bappy71 <19103076@iubat.edu>
2023-01-27 05:44:27 +0600

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"
 import Navbar from './components/Navbar'
 import Home from './components/Home'
 import Register from './components/Register'
+import Edit from "./components/Edit";
 
 
 
@@ -24,6 +25,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/register" component={Register} />
+        <Route exact path="/edit/:id" component={Edit} />
         
       </Switch>
 
