4ab8595379f93af6714c4417ee850748bae88329
Bappy71 <19103076@iubat.edu>
2023-01-27 22:03:12 +0600

    Initial commit

--- client/src/components/Details.js
+++ client/src/components/Details.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import { CardContent } from '@mui/material';
+import imgage from '../img/profile.png'
 
 
 
@@ -10,10 +11,11 @@ const Details = () => {
     return (
         <div className='container mt-3'>
             <h1 className='wcBappy'>Welcome BAPPY!</h1>
+            <img src={imgage} alt="" />
 
             <Card sx={{ minWidth: 275 }}>
                 <CardContent>
-                    
+
                     <h3>Name: <span>BAPPY</span></h3>
                     <h3>Age : <span>26</span></h3>
                 </CardContent>
