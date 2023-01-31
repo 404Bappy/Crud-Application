303b5ef986a5ce6900da5cb2302ee4fea1ba55d2
Bappy71 <19103076@iubat.edu>
2023-02-01 04:27:30 +0600

    Initial commit

--- server/db/connect.js
+++ server/db/connect.js
@@ -2,8 +2,7 @@ const mongoose = require("mongoose");
 const DB = "mongodb+srv://crudApp:kSJ616jt36lKVJh9@cluster0.sz23nml.mongodb.net/?retryWrites=true&w=majority"
 
  mongoose.connect(DB,{
-     userCreateIndex:true,
-     useFindAndModify: false,
+ 
      useNewUrlParser:true,
      useUnifieldTopology:true
  }).then(() => console.log("connection Start")).catch((error) => console.log("error.message"));
