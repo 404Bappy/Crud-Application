e5a71806f301d495907cce2579d6320cfd80ea0b
Bappy71 <19103076@iubat.edu>
2023-02-01 04:10:45 +0600

    Initial commit

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const DB = "mongodb+srv://crudApp:kSJ616jt361KVJh9@cluster0.sz23nml.mongodb.net/?retryWrites=true&w=majority"
 
 const port = 8003;
 
