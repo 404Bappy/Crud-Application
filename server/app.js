d66d963a399b7c5c339baf6951385af39ad7b833
Bappy71 <19103076@iubat.edu>
2023-01-31 22:49:10 +0600

    Initial commit

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const port = 8003;
 
 app.listen(port, () => {
     console.log((`Server is listening on port ${port}`));
 });
+
+
+
+
